782fa93490e194df7f52037716a6184a15a7bfb0
llaulier <lioraloche@yahoo.fr>
2020-03-22 21:02:22 +0100

    sign check

--- exercises/sign.js
+++ exercises/sign.js
@@ -11,6 +11,17 @@
  */
 
 // Your code:
+const sign = (a) =>{
+    if(a > 0){
+        return 1
+    }
+    else if(a < 0){
+        return -1
+    }
+    else if(a === 0){
+        return 0
+    }
+};
 
 //* Begin of tests
 const assert = require('assert');
