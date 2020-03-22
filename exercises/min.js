6bdd222e7b100a7911031432a7d41ca00549ad0a
llaulier <lioraloche@yahoo.fr>
2020-03-22 20:58:45 +0100

    min check

--- exercises/min.js
+++ exercises/min.js
@@ -10,6 +10,17 @@
  */
 
 // Your code:
+const min = (a, b) => {
+    if(a < b){
+        return a
+    }
+    else if(a > b){
+        return b
+    }
+    else if(a === b){
+        return b
+    }
+};
 
 //* Begin of tests
 const assert = require('assert');
