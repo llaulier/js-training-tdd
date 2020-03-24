18b9580cd1312c72e739614621c7dd5c67063410
llaulier <lioraloche@yahoo.fr>
2020-03-24 11:49:02 +0100

    get-first check

--- exercises/get-first.js
+++ exercises/get-first.js
@@ -8,7 +8,9 @@
  */
 
 // Your code:
-
+const getFirst = (arrayOrString) =>{
+   return arrayOrString[0]
+};
 //* Begin of tests
 const assert = require('assert');
 
--- exercises/get-length.js
+++ exercises/get-length.js
@@ -11,10 +11,6 @@
 
 
 const getLength = (arrayOrString) => {
-    const arrStr = {
-        arr: [],
-        str:'',
-    };
     return arrayOrString.length
 };
    
